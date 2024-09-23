05b35baf55011b5f54ac566fdb1bed9da8e3dcc2
roboticswithjulia <julia.marsal.robotics@gmail.com>
2024-09-23 11:06:37 +0200

    Adding cuser in run_user.sh

--- .docker/run_user.sh
+++ .docker/run_user.sh
@@ -1,23 +1,18 @@
 #!/usr/bin/env bash
 
-echo -e "Starting up turtlebot_remote container \n >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-echo -e "This container will access to the users home directory and log in as the user with their password and x sever access.\nYou will not own the workspace though, use sudo chown -R $USER ~/ros2_rust_ws"
-echo -e "Source the workspace with source install/setup.sh"
+cuser_id="1000"
+container_user="cuser"
 
-cuser="1000"
+echo -e "Starting up ros2_rust container \n >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 
 docker run -it --privileged \
-    --user=${cuser} \
+    --user=${cuser_id} \
     --group-add sudo \
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
-    --workdir="/home/$USER/ros2_rust_ws" \
-    --volume="/home/$USER:/home/$USER" \
-    --volume="/etc/group:/etc/group:ro" \
-    --volume="/etc/passwd:/etc/passwd:ro" \
-    --volume="/etc/shadow:/etc/shadow:ro" \
-    --volume="/etc/sudoers.d:/etc/sudoers.d:ro" \
+    --workdir="/home/${container_user}/ros2_rust_ws" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     --net=host \
     --cap-add=sys_nice \
+    --name="ros2_rust_dev" \
     ros2_rust_dev:latest
